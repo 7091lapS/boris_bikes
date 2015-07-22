87d2410905c37f50a57a52d2d170c0c4dc490421
Antonio Voce <antonio.v0c3@gmail.com>
2015-07-22 12:03:45 +0100

    completed challenge 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,10 @@
 require 'docking_station'
+require 'bike'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it "releases a working bike" do
+    bike = subject.release_bike
+    expect(bike.working?).to eq true
+    end
+
 end
