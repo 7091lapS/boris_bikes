08eb1c1c90ce544bb87754c510b721aff1d6a35a
Antonio Voce <antonio.v0c3@gmail.com>
2015-07-24 00:06:48 +0100

    refactored empty/full methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,13 +6,22 @@ class DockingStation
   end
 
   def release_bike
-    raise "No bikes available" if @bikes.empty?
+    raise "No bikes available" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise "Station is full" if @bikes.size >= 20
+    raise "Station is full" if full?
     @bikes << bike
   end
+
+  def empty?
+    @bikes.empty?
+  end
+
+  def full?
+    @bikes.size >= 20
+  end
+
 
 end
